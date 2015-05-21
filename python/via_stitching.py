7141b475e5b9ccdaaecbeaf06e1a655f865206aa
Ben Kempke <bpkempke@gmail.com>
2015-05-20 23:37:52 -0400

    via stitching now does something.  Whether or not it does what it is supposed to is still under debate

--- python/via_stitching.py
+++ python/via_stitching.py
@@ -2,7 +2,7 @@
 import sys
 from pcbnew import *
 import wx
- 
+import numpy as np 
  
 in_filename=sys.argv[1]
 out_filename = sys.argv[2]
@@ -12,18 +12,20 @@ edge_clearance = sys.argv[5]
 via_type_idx = sys.argv[6]
 pcb = LoadBoard(in_filename)
  
+net_code = pcb.FindNet(net_name).GetNet()
  
 pcb_bounding_box = pcb.ComputeBoundingBox()
-pcb_bounding_box.inflate(-FromMils(edge_clearance))
+pcb_bounding_box.Inflate(-FromMils(int(edge_clearance)))
 
-rounded_via_spacing_x = ToMils(pcb_bounding_box.GetWidth())/int(ToMils(pcb_bounding_box.GetWidth())/via_spacing
-rounded_via_spacing_y = ToMils(pcb_bounding_boxGetHeight())/int(ToMils(pcb_bounding_box.GetHeight())/via_spacing
+rounded_via_spacing_x = ToMils(pcb_bounding_box.GetWidth())/int(ToMils(pcb_bounding_box.GetWidth())/float(via_spacing))
+rounded_via_spacing_y = ToMils(pcb_bounding_box.GetHeight())/int(ToMils(pcb_bounding_box.GetHeight())/float(via_spacing))
 
-for x in range(ToMils(pcb_bounding_box.GetX()), ToMils(pcb_bounding_box.GetX() + pcb_bounding_box.GetWidth()), rounded_via_spacing_x):
-    for y in range(ToMils(pcb_bounding_box.GetY()), ToMils(pcb_bounding_box.GetY() + pcb_bounding_box.GetHeight()), rounded_via_spacing_y):
-        print "adding a via..."
+for x in np.arange(ToMils(pcb_bounding_box.GetX()), ToMils(pcb_bounding_box.GetX() + pcb_bounding_box.GetWidth()), rounded_via_spacing_x):
+    for y in np.arange(ToMils(pcb_bounding_box.GetY()), ToMils(pcb_bounding_box.GetY() + pcb_bounding_box.GetHeight()), rounded_via_spacing_y):
+        print "adding a via...%f %f"%(x,y)
         new_via = VIA(pcb)
-        new_via.SetPosition(wx.Point(x,y))
+        new_via.SetPosition(wxPoint(FromMils(float(x)),FromMils(float(y))))
+        new_via.SetNetCode(net_code)
         pcb.Add(new_via)
          
 pcb.Save(out_filename)
