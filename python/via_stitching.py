280b6966bfb5d0ea9ad643c2823277afe6b199f2
Ben Kempke <bpkempke@umich.edu>
2015-05-21 14:17:29 -0400

    via stitching now works.  However, pcbnew seems to decide it wants to ignore the net ids.  Need to figure out how to have that not happen

--- python/via_stitching.py
+++ python/via_stitching.py
@@ -8,24 +8,32 @@ in_filename=sys.argv[1]
 out_filename = sys.argv[2]
 net_name = sys.argv[3]
 via_spacing = sys.argv[4]
-edge_clearance = sys.argv[5]
-via_type_idx = sys.argv[6]
+edge_clearance = FromMils(int(sys.argv[5]))
+via_type_idx = int(sys.argv[6])
 pcb = LoadBoard(in_filename)
  
 net_code = pcb.FindNet(net_name).GetNet()
+
+via_dimensions = pcb.GetViasDimensionsList()[via_type_idx]
  
 pcb_bounding_box = pcb.ComputeBoundingBox()
-pcb_bounding_box.Inflate(-FromMils(int(edge_clearance)))
+left = pcb_bounding_box.GetLeft() + edge_clearance
+right = pcb_bounding_box.GetRight() - edge_clearance
+top = pcb_bounding_box.GetX() + edge_clearance
+bottom = pcb_bounding_box.GetBottom() - edge_clearance
 
-rounded_via_spacing_x = ToMils(pcb_bounding_box.GetWidth())/int(ToMils(pcb_bounding_box.GetWidth())/float(via_spacing))
-rounded_via_spacing_y = ToMils(pcb_bounding_box.GetHeight())/int(ToMils(pcb_bounding_box.GetHeight())/float(via_spacing))
+num_x = int(ToMils(right-left)/float(via_spacing))+1
+num_y = int(ToMils(bottom-top)/float(via_spacing))+1
 
-for x in np.arange(ToMils(pcb_bounding_box.GetX()), ToMils(pcb_bounding_box.GetX() + pcb_bounding_box.GetWidth()), rounded_via_spacing_x):
-    for y in np.arange(ToMils(pcb_bounding_box.GetY()), ToMils(pcb_bounding_box.GetY() + pcb_bounding_box.GetHeight()), rounded_via_spacing_y):
+for x in np.linspace(left, right, num_x):
+    for y in np.linspace(top, bottom, num_y):
         print "adding a via...%f %f"%(x,y)
         new_via = VIA(pcb)
-        new_via.SetPosition(wxPoint(FromMils(float(x)),FromMils(float(y))))
+        new_via.SetViaType(VIA_THROUGH)
+        new_via.SetPosition(wxPoint(x,y))
         new_via.SetNetCode(net_code)
+        new_via.SetWidth(via_dimensions.m_Diameter)
+        new_via.SetDrill(via_dimensions.m_Drill)
         pcb.Add(new_via)
          
 pcb.Save(out_filename)
